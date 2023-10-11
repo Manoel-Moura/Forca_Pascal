b0e0661b1564ea7fd7cae023a45ede018dcfb091
Manoel-Moura <manoelmourabcc@gmail.com>
2023-10-11 11:12:12 -0300

    Jogo da Forca

---
+++ JogoDaForca.pas
@@ -0,0 +1,65 @@
+program JogoDaForca;
+
+var
+  PalavraSecreta, PalavraAdivinhada: string;
+  LetraDigitada: char;
+  TentativasRestantes, i: integer;
+  EncontrouLetra: boolean;
+
+begin
+  //write('Digite a palavra secreta: ');
+  //readln(PalavraSecreta);
+   PalavraSecreta := 'palavrasecreta';
+  
+  TentativasRestantes := Length(PalavraSecreta);
+  PalavraSecreta := Upcase(PalavraSecreta);
+
+  PalavraAdivinhada := '';
+
+  for i := 1 to Length(PalavraSecreta) do
+    PalavraAdivinhada := PalavraAdivinhada + '*';
+
+  writeln('Palavra secreta: ', PalavraAdivinhada);
+
+  while TentativasRestantes > 0 do
+  begin
+    write('Digite uma letra: ');
+    readln(LetraDigitada);
+    LetraDigitada := Upcase(LetraDigitada);
+    EncontrouLetra := false;
+
+    for i := 1 to Length(PalavraSecreta) do
+    begin
+      if PalavraSecreta[i] = LetraDigitada then
+      begin
+        PalavraAdivinhada[i] := LetraDigitada;
+        EncontrouLetra := true;
+      end;
+    end;
+
+    if EncontrouLetra then
+      writeln('A letra "', LetraDigitada, '" pertence à palavra secreta.')
+    else
+    begin
+      writeln('A letra "', LetraDigitada, '" NÃO pertence à palavra secreta.');
+      TentativasRestantes := TentativasRestantes - 1;
+    end;
+
+    writeln('Palavra atual: ', PalavraAdivinhada);
+    writeln('Tentativas restantes: ', TentativasRestantes);
+    writeln('----------------------------------');
+    writeln(' ');
+    
+
+    if PalavraAdivinhada = PalavraSecreta then
+    begin
+      writeln('Parabéns! Você adivinhou a palavra secreta: ', PalavraSecreta);
+      break;
+    end;
+  end;
+
+  if PalavraAdivinhada <> PalavraSecreta then
+    writeln('Você perdeu. A palavra secreta era: ', PalavraSecreta);
+end.
+
+
